49661e0def792e43675c8b39e269ef323226f389
Daniel Gamboa <danielgamboa10@gmail.com>
2021-02-26 11:23:07 -0600

    Add get request to populates color list in BubblePage component

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
+
+  useEffect(() => {
+    getColors()
+  }, [])
+
+  const getColors = () => {
+    axiosWithAuth()
+      .get(`/api/colors`)
+      .then(res => {
+        setColorList(res.data);
+      })
+      .catch(err => {
+        console.log({err});
+      })
+  }
 
   return (
     <>
