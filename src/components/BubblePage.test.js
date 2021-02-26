109da421de783a8db4e8fc6d678f95804a4c6d06
Daniel Gamboa <danielgamboa10@gmail.com>
2021-02-26 12:54:55 -0600

    Create render test for BubblePage component

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -2,12 +2,41 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
 
+const colors = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff",
+    },
+    id: 1,
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc",
+    },
+    id: 2,
+  }
+]
+
+const getColorsMock = jest.fn();
+
 test("Renders BubblePage without errors", () => {
-  // Finish this test
+  render(<BubblePage/>);
 });
 
 test("Fetches data and renders the bubbles on mounting", () => {
-  // Finish this test
+  const { rerender } = render(<BubblePage/>);
+
+  // There should be no colors on first render
+  expect(screen.queryByText(/blueviolet/i)).toBeNull();
+
+  // Re-render component with color data
+  // rerender(<BubblePage/>);
+
+  // Should now have colors
+
+
 });
 
 //Task List
