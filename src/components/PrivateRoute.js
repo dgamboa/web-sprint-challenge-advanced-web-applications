9847baa5a845dda853bf7aeaa1e8fa7eb54f07c5
Daniel Gamboa <danielgamboa10@gmail.com>
2021-02-26 11:10:03 -0600

    Build private route to handle page routes for logged in users

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,11 +1,27 @@
+import React from 'react';
+import { Route, Redirect } from "react-router-dom";
 
-
-
-
-
-
-
-
+export default function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={() => {
+        if (localStorage.getItem("token")) {
+          return <Component />;
+        } else {
+          return (
+            <Redirect
+              to={{
+                pathname: "/",
+                state: { error: "Please log in to access that URL" }
+              }}
+            />
+          );
+        }
+      }}
+    />
+  );
+};
 
 //Task List:
 //1. Build a PrivateRoute component that redirects if user is not logged in
